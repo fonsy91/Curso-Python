05f53bb06a5dcb806c0ba6df5ad900cf317b6426
alfonso91 <alfonso91fp@gmail.com>
2024-05-28 20:41:32 +0200

    BBDD

---
+++ 15.BBDD.py
@@ -0,0 +1,32 @@
+'''
+    Python pude manipular y es compatible con SQL Server, Oracle,
+    Mysql, SQLite, PostgresSQL..., bases de datos relacionales.
+
+    Tambien con bases de datos orientadas a objetos es importante 
+    saber usar el lenguaje de SQL
+
+'''
+import sqlite3
+
+# Abrir-Cerrar conexion
+miConexion = sqlite3.connect("BaseDatosPrueba")
+
+# Crear puntero
+miCursor = miConexion.cursor()
+
+# Ejecutar query SQL
+# Creamos una tabla PRODUCTOS 
+# miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
+
+miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALON',20,'DEPORTES')")
+miConexion.commit()
+
+
+# Manejar los resultados de la query
+
+# Cerrar puntero
+
+# Cerrar conexion
+miConexion.close()
+
+#VAMOS POR EL VIDEO: https://www.youtube.com/watch?v=eM0MkDc34qo&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=56
