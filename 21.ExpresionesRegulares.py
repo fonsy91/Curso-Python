fde5c88383d901307160f489545ecb9dfe5288db
Alfonso Muñoz <alfonsomunoz@MacBook-Pro-de-Alfonso.local>
2024-06-04 10:46:04 +0200

    ExpresionesRegulares Uno

---
+++ 21.ExpresionesRegulares.py
@@ -0,0 +1,8 @@
+'''
+Expresiones regulares: son como una especie de mini lenguaje de programacion 
+dentro de otro lenguaje de programacion. Son una secuencia de caracteres 
+que forman un patron de busqueda. Ejemplos: buscar un texto que se ajusta 
+a un formato determinado (correo electronico), buscar si existe o no una 
+cadena de caracteres dentro de un texto, contar el numero de coincidencias 
+dentro de un texto etc... 
+'''
